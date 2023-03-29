0208ea42f2014839b439a5cf83e17d376060854e
Rebecca Mendes <rebeccacristina.m@gmail.com>
2023-03-29 20:39:42 -0300

    mudança nas cores das classificações

--- App.java
+++ App.java
@@ -17,7 +17,7 @@ public class App {
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = newBuilder(endereco).GET().build();
-    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
         // extrair só os dados que interessam (titulo, poster, classificação) - parciar os dados
@@ -49,39 +49,39 @@ public class App {
         for (double cor = 0; cor <= corClassificacao; cor++) {
 
             if (corClassificacao <= 10.0 && corClassificacao >= 9.7){
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;27;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;15;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
                 break;           
             
             } else if(corClassificacao < 9.7 && corClassificacao >= 9.5) {
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;96;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;20;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
                 break;
 
             } else if (corClassificacao < 9.5 && corClassificacao >= 9.2) {
 
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;140;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;27;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
                 break;
 
             } else if (corClassificacao < 9.2 && corClassificacao >= 9.0) {
 
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;172;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
                
                 break;
                 
             } else if (corClassificacao < 9.0 && corClassificacao >= 8.7) {
 
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;200;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;11;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
                
                 break;
                 
             } else if (corClassificacao < 8.7 && corClassificacao >= 8.5) {
 
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;219;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;68;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
             
                 break;
                 
             } else if (corClassificacao < 8.5 && corClassificacao >= 8.2) {
 
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;187;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
+                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;39;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
             
                 break;
             
@@ -91,31 +91,7 @@ public class App {
             
                 break;
 
-            } else if (corClassificacao < 8.0 && corClassificacao >= 7.7) {
-
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;124;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
-            
-                break;               
-
-            } else if (corClassificacao < 7.7 && corClassificacao >= 7.5) {
-
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;87;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
-            
-                break;
-
-            } else if (corClassificacao < 7.5 && corClassificacao >= 7.2) {
-
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;68;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
-            
-                break; 
-
-            } else if (corClassificacao < 7.2 && corClassificacao >= 7.0) {
-
-                System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;39;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
-            
-                break;  
-
-            } else{
+            } else {
                 System.out.print("\u001b[1m \u001b[38;2;255;255;255m \u001b[48;2;219;11;11mClassificação: " + filme.get("imDbRating") + "\u001b[0m \u001b[m");
             }
         }
